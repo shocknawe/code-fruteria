4b0ccf604d344eb9de8733521ac2cba483f96427
Jairus Ser Aragon <jairus.aragon@zuhlke.com>
2025-07-01 18:44:18 +0800

    fix(#1): add coverage script and fix css import in test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    '/build',
+    '/jest.config.js',
+    '/jest.setup.ts',
+    '/webpack.config.js',
+    // ignore backend for now
+    '/auth/',
+    '/engine/',
+    '/mocks/',
+    '/server/',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'jest-transform-stub',
+  }
 };
